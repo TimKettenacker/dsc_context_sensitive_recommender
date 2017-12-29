f7783efd816b113b01544a5fd028cdb4a268b46d
Tim Kettenacker <timkettenacker@Tims-MBP.fritz.box>
2017-12-29 09:53:11 +0100

    fixed bug to contain only readable pptx-files for ml

--- extract_pptx.R
+++ extract_pptx.R
@@ -3,14 +3,19 @@ install.packages("officer")
 setwd("/Users/timkettenacker/dsproj_repos/R/dsc_context_sensitive_recommender")
 library(officer)
 
-file_name <- list.files(path = "/Users/timkettenacker/Downloads/GDSCDataSet/Presentations")
-pptx_content <- as.data.frame(file_name, stringsAsFactors = FALSE)
+# there is an error in the officer package which causes loops to break if trying to read any other than "pptx", like i.e. outdated "ppt"
+# apparently, it cannot be skipped in tryCatch, that's why it has to be handled beforehand
+files_path <- list.files(path = "/Users/timkettenacker/Downloads/GDSCDataSet/Presentations")
+file_names <- grep("pptx", files_path, value = TRUE, fixed = TRUE)
+didnt_read <- setdiff(files_path, file_names)
+pptx_content <- as.data.frame(file_names, stringsAsFactors = FALSE)
 pptx_content$content <- c("")
-for(i in 1:length(file_name)){
+for(i in 1:length(pptx_content$file_names)){
   tryCatch(
     slides <- data.frame(),
-    slides <- slide_summary(read_pptx(paste0("/Users/timkettenacker/Downloads/GDSCDataSet/Presentations/", file_name[i]))),
-    error=function(e){print(file_name[i])},
+    slides <- slide_summary(read_pptx(paste0("/Users/timkettenacker/Downloads/GDSCDataSet/Presentations/", pptx_content$file_names[i]))),
+    error=function(e){print(pptx_content$file_names[i])},
     pptx_content$content[i] <- paste0(slides$text, collapse = " ")
   )
 }
+
