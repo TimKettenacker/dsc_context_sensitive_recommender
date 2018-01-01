1ac77c88170e0f5c8120ce72a0d82a1b0ec08abf
Tim Kettenacker <timkettenacker@Tims-MBP.fritz.box>
2018-01-01 11:53:21 +0100

    improvement of functions and logical flow

--- build_model.R
+++ build_model.R
@@ -5,5 +5,7 @@ setwd("/Users/timkettenacker/dsproj_repos/R/dsc_context_sensitive_recommender")
 dict <- dictionary(list(search_term = c("BI", "Automotive","Automobiles", "Commercial vehicles")))
 dfm <- dfm(pptx_content$content, remove = stopwords("english"), remove_punct = TRUE, tolower = TRUE)
 lookup <- dfm_lookup(dfm, dict, valuetype = "glob", exclusive = FALSE)
+result_matrix <- as.matrix(lookup)
+result_matrix <- result_matrix[order(result_matrix[,1], decreasing = TRUE),]
 
-# calculate text distances afterwards
+# calculate text distances afterwards or just return ordered list
--- enrich_query.R
+++ enrich_query.R
@@ -6,58 +6,68 @@ require("jsonlite")
 setwd("/Users/timkettenacker/dsproj_repos/R/dsc_context_sensitive_recommender")
 
 args <- commandArgs(trailingOnly = TRUE)
-#args will be a vector of type chr
-#cat(str(args))
+#args is a vector of type chr: cat(str(args))
+industry <- ""
+products <- ""
+related_terms <- ""
 
 #example query:
-args <- c("Solvay","AM","BI","Proposal")
+args <- c("BMW","Strategy","BI","Proposal")
 # the items are already splitted
 # remove "Capgemini" if included in the query
 args <- args[args != "Capgemini"]
 
-# start conditional enrichment of data
-item <- find_item("Solvay", limit = 20)
-
 # checks if any returned item fits a company description
 # and returns the wikidata item
-check4company <- function(item){
-    id <- grep("company", item)
-    company <- get_property(id = item[[id]]$id)
-    return(company)
+check4company <- function(arg){
+  item <- find_item(arg, limit = 10)
+  id <- grep("company|manufacturer|corporation", item)
+  company <- get_property(id = item[[id]]$id)
+  return(company)
 }
 
+get_wikipedia_data <- function(item){
+  wiki_content <- page_content("en", "wikipedia", page_name = item[[1]]$sitelinks$enwiki$title, as_wikitext = TRUE)
+  wiki_content <- gsub("\\[|\\]|\\{|\\}", "", wiki_content$parse$wikitext$`*`) 
+  return(wiki_content)
+}
 
-company_metadata <- tryCatch(
-  check4company(item)
-  , error=function(e){print("No company name found. 
-                            Should be handled more gracefully")})
-
-# from here, a multitude of information is accessible; most notably,
-# company_metadata[[1]]$sitelinks
-# containing the url to the company's wikipedia page
-wiki_content <- tryCatch(
-  page_content("en", "wikipedia", page_name = company_metadata[[1]]$sitelinks$enwiki$title, as_wikitext = TRUE)
-  , error=function(e){print("No sitelink found. Should be handled more gracefully.")})
-
-# get the respective enrichment data from the english wikipedia page
-# by preprocessing the text at first and subsequently
-cleansed_content <- gsub("\\[|\\]|\\{|\\}", "", wiki_content$parse$wikitext$`*`) 
-
-# extracting "industry" and either "product" or "service" as enrichment categories
-find_enrichment_category4industry <- function(cleansed_content){
-  industry_value <- str_extract(cleansed_content, "industry = [:alpha:]+.{0,}")
-  industry_value <- sub("industry = ", "", industry_value)
+# extracting "industry" and "products" as enrichment categories
+find_enrichment_category4industry <- function(wiki_content){
+  industry_value <- str_extract(wiki_content, "industry\\s+= [:alpha:]+.{0,}")
+  industry_value <- sub("industry\\s+=", "", industry_value)
   return(industry_value)
 }
-find_enrichment_category4products <- function(cleansed_content){
-  products_value <- str_extract(cleansed_content, "products = [:alpha:]+.{0,}")
-  products_value <- sub("products = ", "", products_value)
+
+find_enrichment_category4products <- function(wiki_content){
+  products_value <- str_extract(wiki_content, "products\\s+= [:alpha:]+.{0,}")
+  products_value <- sub("products\\s+=", "", products_value)
   return(products_value)
 }
-industry <- find_enrichment_category4industry(cleansed_content)
-products <- find_enrichment_category4products(cleansed_content)
 
-# if no match to a business term on wikipedia could be made, look for related words
-api_link <- paste0("https://api.datamuse.com/words?rel_gen=", "Proposal", "&topics=company&max=3")
-api_out <- fromJSON(api_link)
+search_related_terms <- function(arg){
+  api_link <- paste0("https://api.datamuse.com/words?rel_gen=", arg, "&topics=company&max=3")
+  api_out <- fromJSON(api_link)
+  return(api_out$word)
+}
+
+# start conditional enrichment on user input data
+for(arg in args){
+  print(arg)
+  item <- tryCatch(check4company(arg), error=function(e){})
+  # check if any returned item fits a company description and if so, subsequently add enriching features
+  if(is.null(item) == FALSE){
+    wiki_content <- get_wikipedia_data(item)
+    industry <- find_enrichment_category4industry(wiki_content)
+    products <- find_enrichment_category4products(wiki_content)
+    print(industry)
+    print(products)
+  } # if no match to a business term on wikipedia could be made, look for related words
+  else{
+    related_terms <- search_related_terms(arg)
+    print(related_terms)
+  }
+}
+
+buffed_args <- paste(c(args, industry, products, related_terms))
 
